9256f8786aa570288c97907d01ed6b89aa9b8466
Rafael Rossi <rtrafaeltrova@gmail.com>
2023-02-17 14:57:20 -0300

    fix: container de produtos de usuários centralizado

--- src/paginas/productPages/usrProduct.php
+++ src/paginas/productPages/usrProduct.php
@@ -1,45 +1,47 @@
-<div class="container-fluid container-usrProduct mt-3 px-0 mx-auto">
-    <div class="d-flex row m-0 p-0">
-        <div class="col-lg-4 p-0 m-0 usrProducts-options py-2">
-            <div class="d-flex flex-column">
-                <div class="d-flex row align-items-center justify-content-center m-0">
-                    <div class="col-md-8 col-lg-8 p-0">
-                        <div class="form-floating">
-                            <input type="text" autocomplete="off" class="form-control rounded-0" id="searchInput"
-                                placeholder="Pesquisar" name="search" value="">
-                            <label for="searchInput">Pesquisar</label>
+<div class="container-fluid d-flex justify-content-center align-items-center container-usrPrincipal m-0 p-0">
+    <div class="container-fluid container-usrProduct p-0 m-0">
+        <div class="d-flex row m-0 p-0">
+            <div class="col-lg-3 p-0 m-0 usrProducts-options py-2">
+                <div class="d-flex flex-column">
+                    <div class="d-flex row align-items-center justify-content-center m-0">
+                        <div class="col-md-8 col-lg-8 p-0">
+                            <div class="form-floating">
+                                <input type="text" autocomplete="off" class="form-control rounded-0" id="searchInput"
+                                    placeholder="Pesquisar" name="search" value="">
+                                <label for="searchInput">Pesquisar</label>
+                            </div>
+                        </div>
+
+                        <div class="btn-searchBar col-md-2 col-lg-2 p-0">
+                            <button class="btn btn-primary rounded-0">
+                                <i class="bi bi-search fs-4"></i>
+                            </button>
+                        </div>
+
+                        <div class="col-lg-10 d-flex justify-content-center align-items-center p-0 m-0">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary btn-addProduct rounded-0 w-100" data-bs-toggle="modal" data-bs-target="#modalAddProduto">
+                                <i class="bi bi-plus-circle fs-4"></i>
+                            </button>
                         </div>
                     </div>
-
-                    <div class="btn-searchBar col-md-2 col-lg-2 p-0">
-                        <button class="btn btn-primary rounded-0">
-                            <i class="bi bi-search fs-4"></i>
-                        </button>
+                    <div class="d-flex flex-grow-1 justify-content-center fw-semibold fs-5 align-items-end">
+                        <div class="usrProduct-qte">Quantidade de produtos: <span class="badge bg-primary" id="qte-products">9</span></div>
                     </div>
-
-                    <div class="col-lg-10 d-flex justify-content-center align-items-center p-0 m-0">
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary btn-addProduct rounded-0 w-100" data-bs-toggle="modal" data-bs-target="#modalAddProduto">
-                            <i class="bi bi-plus-circle fs-4"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="d-flex flex-grow-1 justify-content-center fw-semibold fs-5 align-items-end">
-                    <div class="usrProduct-qte">Quantidade de produtos: <span class="badge bg-primary" id="qte-products">9</span></div>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-8 cards-usrProducts p-2">
-            <div class="col-lg-2 m-0 p-0">
-                <a class="card-usrProduct-link" href="produto.php?id=">
-                    <div class="card card-productUsr text-white bg-white rounded-0">
-                        <img class="card-img-top" src="src/img/produto-exemplo.jpg" alt="Title">
-                        <div class="card-body">
-                            <h5 class="card-title">Title dasdasd</h4>
-                            <p class="card-text fs-6">R$ 50,00</p>
+            <div class="col-lg-9 cards-usrProducts p-2">
+                <div class="col-lg-2 m-0 p-0">
+                    <a class="card-usrProduct-link" href="produto.php?id=">
+                        <div class="card card-productUsr text-white bg-white rounded-0">
+                            <img class="card-img-top" src="src/img/produto-exemplo.jpg" alt="Title">
+                            <div class="card-body">
+                                <h5 class="card-title">Title dasdasd</h4>
+                                <p class="card-text fs-6">R$ 50,00</p>
+                            </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
